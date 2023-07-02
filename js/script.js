02b9068042c0e48054111ae3348bb94682e61e96
Xerlok <20791727+Xerlok@users.noreply.github.com>
2023-07-02 21:39:16 +0300

    Add player turn window

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+"use strict";
 const Game = (() => {
     const GameBoard = {
         isGameActive: true,
@@ -20,6 +21,7 @@ const Game = (() => {
             this.activePlayer = this.player[0];
             
             this.cacheDOM();
+            this.playerTurn.innerText = `${this.activePlayer.name} turn `;
             this.bindEvents();
         },
         drawMark: function(row,column) {
@@ -40,28 +42,29 @@ const Game = (() => {
         },
         switchPlayer: function() {
             this.activePlayer = this.activePlayer === GameBoard.player[0] ? GameBoard.player[1] : GameBoard.player[0];
+            this.playerTurn.innerText = `${this.activePlayer.name} turn`;
         },
         checkWinner: function() {
             let player;
-            //rows
-            for (i=0; i <= 2; i++) {
-                if (GameBoard.board[i][0] === '') {continue}
+            //check rows
+            for (let i = 0; i <= 2; i++) {
+                if (GameBoard.board[i][0] === '') {continue};
                 if (GameBoard.board[i][0] === GameBoard.board[i][1] && GameBoard.board[i][1] === GameBoard.board[i][2]) {
                     player = GameBoard.board[i][0];
                     alert('Player ' + player + ' Wins!');
                     this.isGameActive = false;
                 }
             }
-            //columns
-            for (i=0; i <= 2; i++) {
-                if (GameBoard.board[0][i] === '') {continue}
+            //check columns
+            for (let i = 0; i <= 2; i++) {
+                if (GameBoard.board[0][i] === '') {continue};
                 if (GameBoard.board[0][i] === GameBoard.board[1][i] && GameBoard.board[1][i] === GameBoard.board[2][i]) {
                     player = GameBoard.board[0][i];
                     alert('Player ' + player + ' Wins!');
                     this.isGameActive = false;
                 }
             }
-            //diagonals
+            //check diagonals
             if (GameBoard.board[1][1] != '') {
                 if (GameBoard.board[0][0] === GameBoard.board[1][1] && GameBoard.board[1][1] === GameBoard.board[2][2]) {
                     player = GameBoard.board[1][1];
@@ -77,164 +80,66 @@ const Game = (() => {
             //draw?
             let areAllFieldsDrawn;
     
-            for (i = 0; i < 3; i++) {
+            for (let i = 0; i < 3; i++) {
                 areAllFieldsDrawn = GameBoard.board.flat().includes('');
-                if (!areAllFieldsDrawn) {
+                if (!areAllFieldsDrawn && this.isGameActive) {
                     alert('Draw!');
-                    isGameActive = false;
+                    this.isGameActive = false;
                     break;
                 }
             }
         },
         cacheDOM: function() {
-            fields = document.querySelectorAll('.field');
-            restartBtn = document.querySelector('.restart');
+            const fields = document.querySelectorAll('.field');
+            const restartBtn = document.querySelector('.restart');
+            const playerTurn = document.querySelector('.playerTurn');
+
+            this.fields = fields;
+            this.restartBtn = restartBtn;
+            this.playerTurn = playerTurn;
         },
         render: function() {
-            fields[0].innerText = GameBoard.board[0][0];
-            fields[1].innerText = GameBoard.board[0][1];
-            fields[2].innerText = GameBoard.board[0][2];
-            fields[3].innerText = GameBoard.board[1][0];
-            fields[4].innerText = GameBoard.board[1][1];
-            fields[5].innerText = GameBoard.board[1][2];
-            fields[6].innerText = GameBoard.board[2][0];
-            fields[7].innerText = GameBoard.board[2][1];
-            fields[8].innerText = GameBoard.board[2][2];
+            this.fields[0].innerText = GameBoard.board[0][0];
+            this.fields[1].innerText = GameBoard.board[0][1];
+            this.fields[2].innerText = GameBoard.board[0][2];
+            this.fields[3].innerText = GameBoard.board[1][0];
+            this.fields[4].innerText = GameBoard.board[1][1];
+            this.fields[5].innerText = GameBoard.board[1][2];
+            this.fields[6].innerText = GameBoard.board[2][0];
+            this.fields[7].innerText = GameBoard.board[2][1];
+            this.fields[8].innerText = GameBoard.board[2][2];
         },
         bindEvents: function() {
             //squares
             let row;
             let column;
             const self = this;
-            for (i = 0; i <= 8; i++) {
-                fields[i].addEventListener('click', function () {
+            for (let i = 0; i <= 8; i++) {
+                this.fields[i].addEventListener('click', function () {
+                    if (self.activePlayer.number === 1) {
+                        this.style.color = 'red';
+                    }
+                    else if (self.activePlayer.number != 1) {
+                        this.style.color = 'blue';
+                    }
                     row = this.dataset.row;
                     column = this.dataset.column;
                     self.drawMark(row, column);
-                })
+                });
             }
             //restart button
-            restartBtn.addEventListener('click', function () {
-                for (i = 0; i < 3; i++) {
-                    for (j = 0; j < 3; j++) {
+            this.restartBtn.addEventListener('click', function () {
+                for (let i = 0; i < 3; i++) {
+                    for (let j = 0; j < 3; j++) {
                         GameBoard.board[i][j] = '';
                     }
                 }
                 self.isGameActive = true;
                 self.activePlayer = GameBoard.player[0];
                 self.render();
-            })
+            });
         }
-    }
-
-/*     function drawMark (row, column) {
-        if(GameBoard.board[row][column] === '' && isGameActive){
-            if(activePlayer.number === 1){
-                GameBoard.board[row][column] = 'X';
-                render();
-                checkWinner();
-                switchPlayer();
-            }
-            else {
-                GameBoard.board[row][column] = 'O';
-                render();
-                checkWinner();
-                switchPlayer();
-            }
-        }
-    } */
-    
-/*     function switchPlayer() {
-        activePlayer = activePlayer === GameBoard.player[0] ? GameBoard.player[1] : GameBoard.player[0];
-    }     */
-
-   /*  function checkWinner() {
-        let player;
-        //rows
-        for (i=0; i <= 2; i++) {
-            if (GameBoard.board[i][0] === '') {continue}
-            if (GameBoard.board[i][0] === GameBoard.board[i][1] && GameBoard.board[i][1] === GameBoard.board[i][2]) {
-                player = GameBoard.board[i][0];
-                alert('Player ' + player + ' Wins!');
-                isGameActive = false;
-            }
-        }
-        //columns
-        for (i=0; i <= 2; i++) {
-            if (GameBoard.board[0][i] === '') {continue}
-            if (GameBoard.board[0][i] === GameBoard.board[1][i] && GameBoard.board[1][i] === GameBoard.board[2][i]) {
-                player = GameBoard.board[0][i];
-                alert('Player ' + player + ' Wins!');
-                isGameActive = false;
-            }
-        }
-        //diagonals
-        if (GameBoard.board[1][1] != '') {
-            if (GameBoard.board[0][0] === GameBoard.board[1][1] && GameBoard.board[1][1] === GameBoard.board[2][2]) {
-                player = GameBoard.board[1][1];
-                alert('Player ' + player + ' Wins!');
-                isGameActive = false;
-            }
-            else if (GameBoard.board[0][2] === GameBoard.board[1][1] && GameBoard.board[1][1] === GameBoard.board[2][0]) {
-                player = GameBoard.board[1][1];
-                alert('Player ' + player + ' Wins!');
-                isGameActive = false;
-            }
-        }
-        //draw?
-        let areAllFieldsDrawn;
-
-        for (i = 0; i < 3; i++) {
-            areAllFieldsDrawn = GameBoard.board.flat().includes('');
-            if (!areAllFieldsDrawn) {
-                alert('Draw!');
-                isGameActive = false;
-                break;
-            }
-        }
-    } */
-
-/*     function cacheDOM() {
-        fields = document.querySelectorAll('.field');
-        restartBtn = document.querySelector('.restart');
-        return {fields, restartBtn};
-    } */
-
-/*     function render() {
-        fields[0].innerText = GameBoard.board[0][0];
-        fields[1].innerText = GameBoard.board[0][1];
-        fields[2].innerText = GameBoard.board[0][2];
-        fields[3].innerText = GameBoard.board[1][0];
-        fields[4].innerText = GameBoard.board[1][1];
-        fields[5].innerText = GameBoard.board[1][2];
-        fields[6].innerText = GameBoard.board[2][0];
-        fields[7].innerText = GameBoard.board[2][1];
-        fields[8].innerText = GameBoard.board[2][2];
-    } */
-
-/*     function bindEvents() {
-        //squares
-        let row;
-        let column;
-        for (i = 0; i <= 8; i++) {
-            fields[i].addEventListener('click', function () {
-                row = this.dataset.row;
-                column = this.dataset.column;
-                drawMark(row, column);
-            })
-        }
-        //restart button
-        restartBtn.addEventListener('click', function () {
-            for (i = 0; i < 3; i++) {
-                for (j = 0; j < 3; j++) {
-                    GameBoard.board[i][j] = '';
-                }
-            }
-            isGameActive = true;
-            activePlayer = GameBoard.player[0];
-            render();
-        })
-    } */
+    };
     GameBoard.initialize();
 })();
 
